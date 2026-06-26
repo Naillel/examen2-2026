4d5f84b90f979fb3c03c5773c2252b73db7074bd
SrDeiby <deiby.ruiz@ucr.ac.cr>
2026-06-26 14:27:34 -0600

    Se crea el modelo MaterialUnidad

---
+++ app/Models/MaterialUnidad.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MaterialUnidad extends Model
+{
+     protected $table = 'material_unidad';
+    protected $primaryKey = 'idMaterialUnidad';
+
+    protected $fillable = [
+        'cantidad',
+        'idUnidad',
+        'codigo',
+    ];
+
+    public function material()
+    {
+        return $this->belongsTo(Material::class, 'codigo', 'codigo');
+    }
+
+    public function unidad()
+    {
+        return $this->belongsTo(Unidad::class, 'idUnidad', 'idUnidad');
+    }
+
+    public function presupuestos()
+    {
+        return $this->belongsToMany(
+            Presupuesto::class,
+            'material_unidad_presupuesto',
+            'idMaterialUnidad',
+            'codigoPresupuesto'
+        );
+    }
+}
