52d7a0d4bb725e08004db5e0643126c76d4aa2c1
Manuel <manu05gg09@gmail.com>
2026-06-26 14:45:10 -0600

    La ruta del index

--- routes/api.php
+++ routes/api.php
@@ -2,3 +2,4 @@
 
 Route::post('/materiales', [MaterialController::class, 'store']);
 Route::put('/materiales/{id}', [MaterialController::class, 'update']);
+Route::get('/materiales', [MaterialController::class, 'index']);
